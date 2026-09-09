efb53b6bb63df26f798826c325bbca86520727bd
wilsonlf <wilsonluiz2001@gmail.com>
2026-09-08 21:24:03 -0400

    Atividade 3 - Duck

--- entities/player_1.gd
+++ entities/player_1.gd
@@ -1,38 +1,152 @@
 extends CharacterBody2D
 
+enum PlayerState {
+	walk,
+	idle,
+	jump,
+	duck
+}
+
+@onready var sprite: AnimatedSprite2D = $CollisionShape2D/AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+var status: PlayerState
+var direction: float = 0.0
+var jump_count: int = 0
+var max_jump_count: int = 2
+
 const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
 
-@onready var sprite: AnimatedSprite2D = $CollisionShape2D/AnimatedSprite2D
+var original_collision_scale: Vector2
+
+func _ready() -> void:
+	if collision_shape:
+		original_collision_scale = collision_shape.scale
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jump_count = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	match status:
+		PlayerState.idle:
+			idle_state()
+		PlayerState.walk:
+			walk_state()
+		PlayerState.jump:
+			jump_state()
+		PlayerState.duck:
+			duck_state()
+
+	move_and_slide()
+
+func go_to_idle_state() -> void:
+	status = PlayerState.idle
+	sprite.play("idle")
+
+func go_to_walk_state() -> void:
+	status = PlayerState.walk
+	sprite.play("walk")
+
+func go_to_jump_state() -> void:
+	jump_count += 1
+	status = PlayerState.jump
+	sprite.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+func go_to_duck_state() -> void:
+	status = PlayerState.duck
+	
+	if sprite.sprite_frames.has_animation("duck"):
+		sprite.play("duck")
+	else:
+		sprite.play("idle")
+		
+	if collision_shape:
+		collision_shape.scale.y = original_collision_scale.y * 0.5
+
+func exit_from_duck_state() -> void:
+	if collision_shape:
+		collision_shape.scale = original_collision_scale
 
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+func idle_state() -> void:
+	move()
+
+	if Input.is_action_pressed("ui_down"):
+		go_to_duck_state()
+		return
+
+	if Input.is_action_just_pressed("ui_accept"):
+		go_to_jump_state()
+		return
+
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+
+func walk_state():
+	move()
+
+	if Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("ui_accept"):
+		go_to_jump_state()
+		return
+
+	if Input.is_action_pressed("duck") or Input.is_action_pressed("ui_down"):
+		go_to_duck_state()
+		return
+
+	if not is_on_floor():
+		status = PlayerState.jump
+		sprite.play("jump")
+		return
+
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+
+func jump_state() -> void:
+	move()
+
+	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jump_count:
+		go_to_jump_state()
+		return
+
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_walk_state()
+		return
+
+func duck_state() -> void:
+	update_direction()
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if not Input.is_action_pressed("ui_down"):
+		exit_from_duck_state()
+		if direction != 0:
+			go_to_walk_state()
+		else:
+			go_to_idle_state()
+		return
+
+
+func move() -> void:
+	update_direction()
+
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if is_on_floor():
-		if direction > 0:
-			sprite.flip_h = false
-			sprite.play("walk")
-		elif direction < 0:
-			sprite.flip_h = true
-			sprite.play("walk")
-		else:
-			sprite.play("idle")
-	else:
-		sprite.play("jump")
-		
-	move_and_slide()
+func update_direction() -> void:
+	direction = Input.get_axis("ui_left", "ui_right")
+
+	if direction < 0:
+		sprite.flip_h = true
+	elif direction > 0:
+		sprite.flip_h = false
--- entities/transicao_1.gd
+++ entities/transicao_1.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player" and proxima_fase != "":
-		get_tree().change_scene_to_file(proxima_fase)
+		get_tree().call_deferred("change_scene_to_file", proxima_fase)
